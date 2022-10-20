8fcfedbdce615ceee6f3550c8e358d52e9bdbe30
Dominik Magic <dominikmagic5@gmail.com>
2022-10-20 10:51:02 -0700

    WIP: add planet capture to JS
    Co-authored-by: VW <vera.weikel@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Bussines logic
+
+function determineCharacter () {
+  let planet = parseInt(document.querySelector("select#planet").value);
+
+  let result;
+  if (planet === 1) {
+    result = "Earth super strength"
+  } else if (planet === 2) {
+    result = "flying"
+  } 
+}
+
+
+// UI logic
